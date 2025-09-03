0ade64f87084efcc8d47d22faf00e7c296470218
Jcolares34 <jcolares34@gmail.com>
2025-09-03 20:41:37 -0300

    calculadora

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class Calculadora {
+    int somar(int num1, int num2) {
+        return 0;
+    }
+    int subtrair(int num1, int num2) {
+        return 0;
+    }
+    int multiplicar(int num1, int num2) {
+        return 0;
+    }
+    int dividir(int num1, int num2) {
+        return 0;
+    }
+}
